255864b7e6a163ba49b349b1742044452612a46c
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-19 20:08:17 -0800

    second get with id endpoind up & running

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -11,6 +11,7 @@ const Projects = require('./projects-model');
 // `[GET] /api/projects`
 // Returns an array of projects as the body of the response.
 // If there are no projects it responds with an empty array.
+// ??? How come the route localhost:4550/api/projects is not returning any projects???
 router.get('/api/projects', (req, res) => {
     Projects.get()
     .then(projects => {
@@ -23,5 +24,51 @@ router.get('/api/projects', (req, res) => {
         });
     });
 });
+
+
+// `[GET] /api/projects/:id`
+// Returns a project with the given `id` as the body of the response.
+// If there is no project with the given `id` it responds with a status code 404.
+router.get('/api/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    try{
+        const project = await Projects.get(id);
+        if (!project){
+            res.status(404).json({
+                message: 'Thre is no project with the given id'
+            });
+        } else {
+            res.status(200).json(project);
+        }
+
+    }catch (err) {
+        res.status(500).json({
+            Error: {err}
+        });
+    }
+
+})
+
+
+// `[POST] /api/projects`
+// Returns the newly created project as the body of the response.
+// If the request body is missing any of the required fields it responds with a status code 400.
+
+
+// `[PUT] /api/projects/:id`
+// Returns the updated project as the body of the response.
+// If there is no project with the given `id` it responds with a status code 404.
+// If the request body is missing any of the required fields it responds with a status code 400.
+
+
+// `[DELETE] /api/projects/:id`
+// Returns no response body.
+// If there is no project with the given `id` it responds with a status code 404.
+
+
+// `[GET] /api/projects/:id/actions`
+// Returns an array of actions (could be empty) belonging to a project with the given `id`.
+// If there is no project with the given `id` it responds with a status code 404.
+
 
 module.exports = router;
