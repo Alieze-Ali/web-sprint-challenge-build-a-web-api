8894e38a410304f93ab861d526630765b2e85509
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-19 19:43:34 -0800

    first endpoint get working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,4 +3,25 @@ const express = require('express');
 const router = express.Router();
 const Projects = require('./projects-model');
 
+
+// MIDDLEWARE
+
+// ROUTES
+
+// `[GET] /api/projects`
+// Returns an array of projects as the body of the response.
+// If there are no projects it responds with an empty array.
+router.get('/api/projects', (req, res) => {
+    Projects.get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'No projects could be found',
+        });
+    });
+});
+
 module.exports = router;
