d99856c09d2b1c8d6f2e9b856fbb9293b09e1766
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-19 19:18:42 -0800

    first commit, server running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const server = express();
-
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+const express = require('express');
+const morgan = require("morgan");
+const helmet = require("helmet");
+const server = express();
+
+// Middleware
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+
+server.use(express.json());
+server.use(projectsRouter);
+server.use(actionsRouter);
+
+
+const currentTime = new Date().toLocaleTimeString();
+
+server.get("/status", (req,res)=>{
+    res.status(200).json({message:"hello from node", currentTime});
+});
 
 module.exports = server;
