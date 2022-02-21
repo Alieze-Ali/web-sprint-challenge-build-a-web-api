1a1537253990c443aa7da122d137e918988614d5
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-02-20 17:53:13 -0800

    added more middleware

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -29,7 +29,7 @@ router.get('/api/actions', (req, res) => {
 // `[GET] /api/actions/:id` - SAME
 // Returns an action with the given `id` as the body of the response.
 // If there is no action with the given `id` it responds with a status code 404.
-router.get('/api/actions/:id', async (req, res) => {
+router.get('/api/actions/:id', validateActionId, async (req, res) => {
     const { id } = req.params;
     try{
         const action = await Actions.get(id);
@@ -72,7 +72,7 @@ router.post('/api/actions', (req, res) => {
 // Returns the updated action as the body of the response.
 // If there is no action with the given `id` it responds with a status code 404.
 // If the request body is missing any of the required fields it responds with a status code 400.
-router.put('/api/actions/:id', (req,res) => {
+router.put('/api/actions/:id', validateActionId, validateAction, (req,res) => {
     const id = req.params.id;
     const changes = req.body;
     if (!changes) {
@@ -101,7 +101,7 @@ router.put('/api/actions/:id', (req,res) => {
 // `[DELETE] /api/actions/:id` - SAME
 // Returns no response body.
 // If there is no action with the given `id` it responds with a status code 404.
-router.delete('/api/actions/:id', async (req, res) => {
+router.delete('/api/actions/:id', validateActionId, async (req, res) => {
     const { id } = req.params;
     try{
       const deletedAction = await Actions.remove(id);
